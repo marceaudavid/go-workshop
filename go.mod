e4f76d600b1c7a157d5e84e753c01e08b0e6b277
marceaudavid <marceau.david@ynov.com>
2020-01-14 08:14:08 +0100

    Working on save function

--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/gorilla/securecookie v1.1.1
 	github.com/gorilla/sessions v1.2.0
-	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/joho/godotenv v1.3.0
 )
--- server.go
+++ server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"os"
@@ -12,6 +14,7 @@ import (
 
 	"github.com/gorilla/securecookie"
 	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
 )
 
 var key = securecookie.GenerateRandomKey(32)
@@ -31,15 +34,21 @@ type User struct {
 }
 
 func main() {
-	DSN := os.Getenv("DSN")
-	db, err := sql.Open("mysql", DSN)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := sql.Open("mysql", os.Getenv("DSN"))
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
 
 	// db.Exec("DROP TABLE IF EXISTS users")
-	// db.Exec("CREATE TABLE IF NOT EXISTS users (username VARCHAR(255), password VARCHAR(255), email VARCHAR(255))")
+	db.Exec("CREATE TABLE IF NOT EXISTS users (username VARCHAR(255), password VARCHAR(255), email VARCHAR(255))")
+	// db.Exec("DROP TABLE IF EXISTS data")
+	db.Exec("CREATE TABLE IF NOT EXISTS data (username VARCHAR(255), data JSON)")
 
 	http.HandleFunc("/random", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "%f", rand.Float64())
@@ -63,6 +72,7 @@ func main() {
 				} else {
 					session, _ := store.Get(r, "auth")
 					session.Values["authenticated"] = true
+					session.Values["username"] = user.Username
 					session.Save(r, w)
 					fmt.Fprintf(w, "OK")
 				}
@@ -77,7 +87,6 @@ func main() {
 			err := dec.Decode(&subscriber)
 			if err != nil {
 				http.Error(w, "Bad Request", http.StatusBadRequest)
-				fmt.Fprintf(w, "%s", err.Error())
 			} else {
 				var username string
 				err := db.QueryRow("SELECT username FROM users WHERE username = ?", subscriber.Username).Scan(&username)
@@ -97,6 +106,27 @@ func main() {
 		}
 	})
 
+	http.HandleFunc("/save", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			session, _ := store.Get(r, "auth")
+			auth, ok := session.Values["authenticated"].(bool)
+
+			if !auth || !ok {
+				buf := new(bytes.Buffer)
+				buf.ReadFrom(r.Body)
+				json := buf.String()
+				user, _ := session.Values["username"]
+				res, err := db.Exec("UPDATE INTO data VALUES (?, ?)", user, json)
+				_ = res
+				if err != nil {
+					http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				} else {
+					fmt.Fprintf(w, json)
+				}
+			}
+		}
+	})
+
 	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
 		session, _ := store.Get(r, "auth")
 		auth, ok := session.Values["authenticated"].(bool)
@@ -110,6 +140,7 @@ func main() {
 	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
 		session, _ := store.Get(r, "auth")
 		session.Values["authenticated"] = false
+		session.Values["username"] = nil
 		session.Save(r, w)
 		fmt.Fprintf(w, "Vous avez été deconnecté")
 	})
